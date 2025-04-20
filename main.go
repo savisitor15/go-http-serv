4edefe5ad008aa5fd2a0ae91ebabf0709e8a52db
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2025-04-20 16:40:03 +0200

    Added some handlers for paths

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir("./static")))
+	serveMux.Handle("/assets", http.FileServer(http.Dir("./static/assets")))
 	server := http.Server{Handler: serveMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
