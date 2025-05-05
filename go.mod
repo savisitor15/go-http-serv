fe329f3647b59fab472969d0fc23f4ef3663432f
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2025-05-05 21:41:24 +0200

    implemented JWT handling

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/savisitor15/go-http-serv
 go 1.24.2
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0 // direct
 	github.com/joho/godotenv v1.5.1 // direct
 	github.com/lib/pq v1.10.9 // direct
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
@@ -12,4 +18,32 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	ts := time.Now().UTC()
+	start := jwt.NewNumericDate(ts)
+	end := jwt.NewNumericDate(ts.Add(expiresIn))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{Issuer: "chirpy", IssuedAt: start, ExpiresAt: end, Subject: userID.String()})
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString, 
+		&jwt.RegisteredClaims{}, 
+		func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret),nil},
+	)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	rawId, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	uid, err := uuid.Parse(rawId)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return uid, nil
 }
